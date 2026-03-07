e07a4053abba22d19a0a8da1b13003c2d31372f9
Evgeniy Pronyakin <ypronyakin@internet.ru>
2026-03-07 08:43:43 +0300

    Created DLL

---
+++ ConnectorCheck/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConnectorCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=\"Movies_SPU 411\";Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Connector. Connector connection = new Connector.Connector (connection_string);
+
+            connection.Select("SELECT * FROM Directors");
+        }
+    }
+}
